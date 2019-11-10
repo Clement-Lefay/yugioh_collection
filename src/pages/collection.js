7757c5b268981d7636d8ebb072150c8bfcaceda8
Clement Le Fay <clement.le.fay@gmail.com>
2019-11-10 23:24:44 +0100

    WIP - update the broken link of images to letter R

--- src/pages/collection.js
+++ src/pages/collection.js
@@ -57,7 +57,7 @@ function CardIterationDisplay(data) {
     return a.name.localeCompare(b.name)
   })
 
-  cardItemArraySorted.slice(700, 750).forEach(card => {
+  cardItemArraySorted.slice(1250, 1300).forEach(card => {
     cardItemArray.push(
       <tr key={card.cardId}>
         <td>
